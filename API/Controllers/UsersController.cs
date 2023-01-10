17195b4cfb89cf0ac657e2a28963d50a5a93da2f
unknown <aurimas.jasius@gmail.com>
2023-01-10 20:49:54 +0200

    Create API UsersController

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // GET /api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
