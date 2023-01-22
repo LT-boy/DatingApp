1a746f0045e6a4a3fb9fc3a25d270a4b9c36bcd1
Aurimas <aurimas.jasius@gmail.com>
2023-01-22 13:18:27 +0200

    Add extension methods

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,24 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    //When we use static, we don't need to instantiate methods in that class
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors();
+            //Building service for token (specify interface and implementation class)
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+    }
+}
