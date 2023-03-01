0ad84a5ed94654d1b698bcac61cbd022b048d4fc
Aurimas <aurimas.jasius@gmail.com>
2023-03-01 20:43:53 +0200

    Add styling to cards

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 
 @Component({
